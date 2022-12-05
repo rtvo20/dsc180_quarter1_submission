9f13e19d1f35253f56286d549e8018358de6646c
rtvo20 <rtvo@ucsd.edu>
2022-12-05 11:47:33 -0800

    pushing files

--- src/action_feature.py
+++ src/action_feature.py
@@ -227,7 +227,7 @@ def save_action_csvs(data):
     act_cols =  ['step_id','action','chemical_from','drop_air_gap','drop_blow_out','drop_height','drop_rate','spin_acc','spin_rpm','hp_duration','hp_temp','rest_duration','char_name','char_exposure time','char_duration','char_position','sample_id','batch_id']
     dfs = []
     for sample in data['samples']:
-        act = pd.DataFrame(action_table(sample, sample['name'], batch_id), columns=act_cols)
+        act = pd.DataFrame(action_table(sample, sample['name'], batch_id))
         df = act.astype({'chemical':'Int64'})
         dfs.append(df)
         fname = batch_id + '_' + sample + '_action.csv'
