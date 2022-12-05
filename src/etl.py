7aed23d3f9e1aed1a72601eaea6220e355a0be97
rtvo20 <rtvo@ucsd.edu>
2022-12-05 15:34:09 -0800

    pushing files

--- src/etl.py
+++ src/etl.py
@@ -16,7 +16,7 @@ def get_data():
 
 def get_ids():
     fp = "test/testdata/test_ids.csv"
-    id_csv = pd.read_csv(fp, skiprows=1)
+    id_csv = pd.read_csv(fp, skiprows=0)
     drop_cols = id_csv.columns[3:6].to_list() + [id_csv.columns[7]]
     id_csv = id_csv.drop(columns=drop_cols)
     id_csv.columns = ['batch_id', 'sample_id', 'file_type', 'file_id']
